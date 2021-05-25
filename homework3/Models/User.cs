3ea417dc3cbb7c78315f477bf153903d97dd7f9b
mchoman193 <83388488+mchoman193@users.noreply.github.com>
2021-05-25 16:40:42 -0400

    Add files via upload

---
+++ homework3/Models/User.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace HelloWorld.Models
+{
+    class User : IDataErrorInfo, INotifyPropertyChanged
+    {
+        private string name = string.Empty;
+        private string password = string.Empty;
+        private string nameError;
+        private string passwordError;
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        // This represents the error string for a uxName data validation.  This string
+        // is provided via the IDataErrorInfo interface and will be either:
+        //     "Name cannot be empty." or
+        //     "Name cannot be longer than 12 characters.".
+        public string NameError
+        {
+            get
+            {
+                return nameError;
+            }
+            set
+            {
+                if (nameError != value)
+                {
+                    nameError = value;
+                    OnPropertyChanged("NameError");
+                }
+            }
+        }
+
+        public string PasswordError
+        {
+            get
+            {
+                return passwordError;
+            }
+            set
+            {
+                if (passwordError != value)
+                {
+                    passwordError = value;
+                    OnPropertyChanged("PasswordError");
+                }
+            }
+        }
+
+        // Getter and setter for the User.cs name field.
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        // Getter and setter for the User.cs password field.
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged("Password");
+                }
+            }
+        }
+
+        // IDataErrorInfo interface
+        public string Error
+        {
+            get
+            {
+                // return NameError;
+                return null;
+            }
+        }
+
+        // This serves as our data validation event.  It is called when
+        // ValidatesOnDataErrors=True in MainWindow.xaml is set.
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case "Name":
+                    {
+                        NameError = "";
+                        if (string.IsNullOrEmpty(Name))
+                        {
+                            NameError = "Name cannot be empty.";
+                        }
+                        if (Name.Length > 12)
+                        {
+                            NameError = "Name cannot be longer than 12 characters.";
+                        }
+                        return NameError;
+                    }
+                    case "Password":
+                    {
+                        PasswordError = "";
+                        if (string.IsNullOrEmpty(Password))
+                        {
+                            PasswordError = "Password cannot be empty.";
+                        }
+                        return PasswordError;
+                    }
+                }
+                return null;
+            }
+        }
+
+        // INotifyPropertyChanged interface.
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This triggers the above IDataErrorInfo interface data validations to occur.  It
+        // occurs when a property value changes.  The event is processed in XAML, which updates
+        // any fields that is bound to it, such as NameError. 
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ homework3/SecondWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        CollectionView view;
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+        public SecondWindow()
+        {
+            InitializeComponent();
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
+
+            // Set a default sort order for the ListView.
+            uxList.ItemsSource = users;
+            view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+        }
+
+        // Apply sorting to the selected column heading.  In addition, append a sorting
+        // image adjacent the column heading.  This indicates an ascending or descending
+        // sort.
+        private void uxUsersColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            var users = new List<Models.User>();
+
+            // Obtain which column heading was clicked.
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                uxList.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            view.SortDescriptions.Clear();
+
+            // Apply the sorting.
+            uxList.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+        }
+    }
+}
